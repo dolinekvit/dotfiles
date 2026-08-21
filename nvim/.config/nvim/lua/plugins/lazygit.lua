09fac3ccbddfd9cbe24d410067f3f7ab3274459e
vitdolinek <dolinekvit@gmail.com>
2026-08-21 11:12:31 +0200

    change: lazygit now loads config from `~/.config/lazygit`, lazygit active line color changed to light gray

--- nvim/.config/nvim/lua/plugins/lazygit.lua
+++ nvim/.config/nvim/lua/plugins/lazygit.lua
@@ -14,4 +14,9 @@ return {
     { "<leader>gl", "<cmd>LazyGit<cr>", desc = "LazyGit" },
     { "<leader>gf", "<cmd>LazyGitCurrentFile<cr>", desc = "LazyGit (current file history)" },
   },
+  init = function()
+    vim.g.lazygit_use_custom_config_file_path = 1
+    vim.g.lazygit_config_file_path =
+      vim.fn.expand("~/.config/lazygit/config.yml")
+  end,
 }
